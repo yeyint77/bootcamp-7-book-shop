f85d23866ba6b96951dfaeaa3ac568c269a59712
yeyint <yeyintmyatmin77@gmail.com>
2024-01-08 11:31:38 +0630

    initial commit

---
+++ src/main/java/com/example/booksshop/BooksShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.booksshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/booksshop/BooksShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booksshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
